10a87d40b1a52e89a700db5acc96e9083a5635de
Hathoo <dzinek12@gmail.com>
2017-03-22 08:37:40 +0100

    2017-03-22 poprawka

--- string2.py
+++ string2.py
@@ -59,13 +59,14 @@ def front_back(a, b):
   # +++your code here+++
   # LAB(begin solution)
   # Figure out the middle position of each string.
-  a_middle = len(a) / 2
-  b_middle = len(b) / 2
-  if len(a) % 2 == 1:  # add 1 if length is odd
-    a_middle = a_middle + 1
-  if len(b) % 2 == 1:
-    b_middle = b_middle + 1 
-  return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
+   a_middle = int(len(a) / 2)
+   b_middle = int(len(b) / 2)
+   if len(a) % 2 == 1:
+     a_middle = a_middle + 1
+   if len(b) % 2 == 1:
+     b_middle = b_middle + 1
+   ble = a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
+   return ble
   # LAB(replace solution)
   # return
   # LAB(end solution)
